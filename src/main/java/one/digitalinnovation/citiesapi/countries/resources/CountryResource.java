de6e2f66b9c7b6b0ecb8c97b4e6f59b84fcdcb05
marianos <marianos8125@gmail.com>
2021-09-10 21:50:15 -0300

    add endpoint for CountryResource

--- src/main/java/one/digitalinnovation/citiesapi/countries/resources/CountryResource.java
+++ src/main/java/one/digitalinnovation/citiesapi/countries/resources/CountryResource.java
@@ -2,23 +2,40 @@ package one.digitalinnovation.citiesapi.countries.resources;
 
 import one.digitalinnovation.citiesapi.countries.entities.Country;
 import one.digitalinnovation.citiesapi.countries.repositories.CountryRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import java.util.Optional;
 
 @RestController
+@RequestMapping("/countries")
 public class CountryResource {
 
-  private final CountryRepository repository;
+    private final CountryRepository repository;
 
-  public CountryResource(final CountryRepository repository) {
-    this.repository = repository;
-  }
+    public CountryResource(final CountryRepository repository) {
+        this.repository = repository;
+    }
 
-  @GetMapping("/countries")
-  public List<Country> cities() {
+    @GetMapping
+    public Page<Country> cities(Pageable page) {
 
-    return repository.findAll();
-  }
+        return repository.findAll(page);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Country> getOne(@PathVariable Long id) {
+        Optional<Country> optional = repository.findById(id);
+
+        if (optional.isPresent()) {
+            return ResponseEntity.ok().body(optional.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
